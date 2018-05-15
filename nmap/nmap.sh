fe6f40248c0e5d1a15d939a1549927ab9e097be7
katia-e <ekaterina@gig.tech>
2018-05-15 12:36:23 +0000

    add nmap buld script

---
+++ nmap/nmap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+apt-get update
+apt-get install -y build-essential
+
+curl -O https://nmap.org/dist/nmap-7.70.tar.bz2
+bzip2 -cd nmap-7.70.tar.bz2 | tar xvf -
+cd nmap-7.70
+./configure
+make
+
+mkdir -p /tmp/archives/
+tar cvfz "/tmp/archives/nmap.tar.gz" --transform "s,^,usr/bin/," nmap
